eae0eb62754abcef6bddbdef6892a25cc514f03a
Lucas Sánchez <lucasgsanchez@gmail.com>
2021-02-22 20:35:23 -0300

    Adds some dependencies: koin, coroutines and retrofit

---
+++ app/src/main/java/com/dendron/redditclient/MainApplication.kt
@@ -0,0 +1,18 @@
+package com.dendron.redditclient
+
+import android.app.Application
+import com.dendron.redditclient.di.applicationModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MainApplication: Application(){
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@MainApplication)
+            androidLogger()
+            modules(applicationModule)
+        }
+    }
+}
